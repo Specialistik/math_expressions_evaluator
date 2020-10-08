139bfbbc2d8dd696c32c0b776013a848e1f4eb62
Specialistik <test04@list.ru>
2020-10-09 02:15:26 +0300

    broken things to check the initial one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Evaluator from './evaluator/formula_evaluator';
 
 function App() {
+  function evaluate(e) {
+    e.preventDefault();
+    console.log(document.getElementById('expression').value)
+    var result = Evaluator.eval(document.getElementById('expression').value.trim());
+    console.log('the result value is ', result);
+    document.getElementById('result').innerHTML = result;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>A react calculator application</h1>
+        <input name="expression" id="expression" placeholder='Enter an expression and press "calculate" button'></input>
+        <button onClick={evaluate} className="btn">
+          <span>calculate</span>
+        </button>
+        <h1 id="result">Result expression</h1>
       </header>
     </div>
   );
